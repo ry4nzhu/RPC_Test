5cf30857cba1df6eb7a0e930d08a82a96f1c12ad
Ruiyang Zhu <ryanzhu@umich.edu>
2019-04-27 19:13:22 -0400

    Initial Commit

---
+++ RPC_Test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  RPC_Test
+//
+//  Created by 祝瑞洋 on 2019/4/27.
+//  Copyright © 2019 ryanzhu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
